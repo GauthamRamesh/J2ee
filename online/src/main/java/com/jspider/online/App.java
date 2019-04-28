06205b4c5808f3f3564cd15487b113e0e0e1b495
unknown <gautham.r92@gmail.com>
2019-04-28 20:10:44 +0530

    first push

---
+++ online/src/main/java/com/jspider/online/App.java
@@ -0,0 +1,14 @@
+package com.jspider.online;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String s1="Adam";
+        System.out.println( "Hello World!" +s1);
+    }
+}
