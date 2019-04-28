ccf6070659ace2035ccaade7829c58f4ef69ecfc
unknown <gautham.r92@gmail.com>
2019-04-28 20:15:10 +0530

    second push

--- online/src/main/java/com/jspider/online/App.java
+++ online/src/main/java/com/jspider/online/App.java
@@ -6,9 +6,13 @@ package com.jspider.online;
  */
 public class App 
 {
+	
+	//git config --global user.email "gautham.r92@gmail.com"
     public static void main( String[] args )
     {
     	String s1="Adam";
+    	String s2="Gautham";
+
         System.out.println( "Hello World!" +s1);
     }
 }
---
+++ online/src/main/java/com/jspider/online/TestApp.java
@@ -0,0 +1,7 @@
+package com.jspider.online;
+
+public class TestApp {
+public static void main(String[] args) {
+	
+}
+}
